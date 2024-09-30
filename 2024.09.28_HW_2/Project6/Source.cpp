d87709b516cae22d623274ce6e65b241229055b9
st128757 <st128757@student.spbu.ru>
2024-09-30 22:59:00 +0300

    HW2 upload 2 except boxes

--- 2024.09.28_HW_2/Project6/Source.cpp
+++ 2024.09.28_HW_2/Project6/Source.cpp
@@ -1,7 +1,8 @@
 #include <cstdio>
 #include <cmath>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     double a = 0;
     double b = 0;
     double c = 0;
@@ -11,22 +12,27 @@ int main(int argc, char* argv[]) {
     scanf_s("%lf", &c);
 
     double D = b * b - 4 * a * c;
-    if (a == b && b == c && c == 0) {
+    if (a == b && b == c && c == 0)
+    {
         printf("-1");
     }
-    else if (a == b && b == 0) {
+    else if (a == b && b == 0)
+    {
         printf("0");
     }
-    else if (a == 0) {
+    else if (a == 0)
+    {
         printf("1\n");
         printf("%f", -1.0 * c / b);
     }
-    else if (D > 0) {
+    else if (D > 0)
+    {
         printf("2\n");
         printf("%f\n", (-1.0 * b - sqrt(D)) / (2.0 * a));
         printf("%f", (-1.0 * b + sqrt(D)) / (2.0 * a));
     }
-    else if (D == 0) {
+    else if (D == 0)
+    {
         printf("1\n");
         printf("%f", (-1.0 * b) / (2.0 * a));
     }
